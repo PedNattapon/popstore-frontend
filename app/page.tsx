9c8816382272ffce15994d884fbb8333643d81de
It's me Ped <pedntpin20111@gmail.com>
2025-03-31 21:58:15 +0700

    chore(search): adjust input width to full

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Button from "@/components/ui/Button"
 
 const page = () => {
   return (
    <>
    
    <div>
-    <Button>page</Button>
    </div>
    </>
   )
--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -8,50 +8,50 @@ const Search = () => {
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(70);
     const period = 5000;
-
+  
     useEffect(() => {
-        const ticker = setTimeout(() => {
-            tick();
-        }, delta);
+      const ticker = setTimeout(() => {
+        tick();
+      }, delta);
 
-        return () => clearTimeout(ticker);
+      return () => clearTimeout(ticker);
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [text, delta]);
-
+  
     const tick = () => {
-        const i = loopNum % toRotate.length;
-        const fullText = toRotate[i];
-        const updatedText = isDeleting
-            ? fullText.substring(0, text.length - 1)
-            : fullText.substring(0, text.length + 1);
-
-        setText(updatedText);
-
-        if (isDeleting) {
-            setDelta(70); 
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true);
-            setDelta(period); 
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false);
-            setLoopNum(loopNum + 1);
-            setDelta(70); 
-        }
+      const i = loopNum % toRotate.length;
+      const fullText = toRotate[i];
+      const updatedText = isDeleting
+        ? fullText.substring(0, text.length - 1)
+        : fullText.substring(0, text.length + 1);
+  
+      setText(updatedText);
+  
+      if (isDeleting) {
+        setDelta(70);
+      }
+  
+      if (!isDeleting && updatedText === fullText) {
+        setIsDeleting(true);
+        setDelta(period);
+      } else if (isDeleting && updatedText === '') {
+        setIsDeleting(false);
+        setLoopNum(loopNum + 1);
+        setDelta(70);
+      }
     };
-
-  return (
-    <div className="flex items-center border-[3px] border-b-7 border-black rounded-[12px] shadow-md px-3 py-0.5 text-xl">
-        <input
-        type="text"
-        placeholder={text}
-        className="outline-none placeholder-gray-400 bg-transparent w-26"
-        />
-        <div className="border-l border-black h-6 mx-3"></div>
-        <button className="text-sm underline hover:text-gray-500">Search</button>
+  
+    return (
+        <div className="flex items-center border-[3px] border-b-7 border-black rounded-[12px] shadow-md px-3 py-0.5 text-xl">
+          <input
+            type="text"
+            placeholder={text}
+        className="outline-none placeholder-gray-400 bg-transparent w-full"
+          />
+          <div className="border-l border-black h-6 mx-3"></div>
+          <button className="text-sm underline hover:text-gray-500">Search</button>
         
-    </div>
+      </div>
   )
 }
 export default Search
